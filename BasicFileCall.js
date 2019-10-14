b27a104b0bd14aed1a5c9c69624549d14c077470
JaweedShuja <56365178+JaweedShuja@users.noreply.github.com>
2019-10-14 13:24:12 +0500

    Create BasicFileCall.js
    In this code example you will learn to call another file in which there is method sum which is use to sum two numbers

---
+++ BasicFileCall.js
@@ -0,0 +1,9 @@
+const sum = (num1, num2) => num1 + num2;
+module.exports = sum
+
+/*In another file eg app.js write this code to access sum Method
+
+//assuming file is in same path
+const BasicFileCall = require('./BasicFileCall.js');
+
+console.log(tutorial(1,1));*/
